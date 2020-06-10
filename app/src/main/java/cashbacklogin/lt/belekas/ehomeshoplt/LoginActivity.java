07ec91af3dfb7b086fd5fd83be4fcf222d63a9a8
KestutisRuockus <kestutisruockus@gmail.com>
2020-06-10 11:16:58 +0300

    Added: - Register User UI - Register Seller UI - Login UI - Forgot password UI

--- app/src/main/java/cashbacklogin/lt/belekas/ehomeshoplt/LoginActivity.java
+++ app/src/main/java/cashbacklogin/lt/belekas/ehomeshoplt/LoginActivity.java
@@ -1,15 +1,46 @@
 package cashbacklogin.lt.belekas.ehomeshoplt;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
+    // UI views
+    private EditText emailEt, passwordEt;
+    private TextView forgotTv, noAccountTv;
+    private Button loginBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        // Init UI views
+        emailEt = findViewById(R.id.emailEt);
+        passwordEt = findViewById(R.id.passwordEt);
+        forgotTv = findViewById(R.id.forgotTv);
+        noAccountTv = findViewById(R.id.noAccountTv);
+        loginBtn = findViewById(R.id.loginBtn);
+
+        noAccountTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(LoginActivity.this, RegisterUserActivity.class));
+            }
+        });
+
+        forgotTv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity( new Intent(LoginActivity.this, ForgotPasswordActivity.class));
+            }
+        });
 
     }
 }
