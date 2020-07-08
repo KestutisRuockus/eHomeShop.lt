512c157bf61a727fda5022d9817a9031dbaba3a8
KestutisRuockus <kestutisruockus@gmail.com>
2020-07-08 12:59:31 +0300

    Created: - Add product Stage.

---
+++ app/src/main/java/cashbacklogin/lt/belekas/ehomeshoplt/Constants.java
@@ -0,0 +1,21 @@
+package cashbacklogin.lt.belekas.ehomeshoplt;
+
+public class Constants {
+
+    // product categories
+   public static final String[] productCategories = {
+            "Beverages",
+            "Beauty & Personal Care",
+            "Baby Kids",
+            "Biscuits Snacks & Chocolates",
+            "Breakfast & Dairy",
+            "Cooking Needs",
+            "Frozen Food",
+            "Fruits",
+            "Pet Cate",
+            "Pharmacy",
+            "Vegetables",
+            "Others"
+    };
+
+}
